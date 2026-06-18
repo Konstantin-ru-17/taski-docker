3618b850854928db54bf0687535cd892553a889f
Konstantin <kakolesnikovru@gmail.com>
2026-06-18 16:57:11 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','158.160.153.37', 'task-for.myftp.org','127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '158.160.153.37', 'task-for.myftp.org', '127.0.0.1']
 
 # Application definition
 
@@ -117,7 +116,7 @@ STATIC_URL = '/static/'
 # в контейнере это будет /app/collected_static
 STATIC_ROOT = BASE_DIR / 'collected_static'
 # Теперь при вызове команды python manage.py collectstatic
-# Django будет копировать все статические файлы в директорию collected_static 
+# Django будет копировать все статические файлы в директорию collected_static
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
